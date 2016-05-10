90d0d4cdf5f09361e23d3f91104c4bdce3068172
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-05-10 01:22:20 -0400

    add post endpoint for temperature

--- server.go
+++ server.go
@@ -7,16 +7,37 @@ import (
     "math/rand"
     "time"
     "encoding/json"
+    "database/sql"
+
     "github.com/gorilla/mux"
     "github.com/rs/cors"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+    db, err := sql.Open("sqlite3", "temp.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
     router.HandleFunc("/temp", Temperature)
-    fmt.Printf("Listening on port: 8080\n")
-    log.Fatal(http.ListenAndServe(":8080", cors.Default().Handler(router)))
+    router.HandleFunc("/temp/add/{temp}/{time}", AddTemp).Methods("POST")
+    fmt.Printf("Listening on port: 80\n")
+    log.Fatal(http.ListenAndServe(":80", cors.Default().Handler(router)))
+}
+
+func AddTemp(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    time := vars["time"]
+    temp := vars["temp"]
+    fmt.Println(time, ":", temp)
+
+    // add to database
+    _, err := db.Exec("insert into temp(time, temp) values (%s, %s)", time, temp)
+    if err != nil { log.Fatal(err) }
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
